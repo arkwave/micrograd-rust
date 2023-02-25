627d62b4a02c7eb59090b25343cbc0129fd1fc3e
Ananth Ravi Kumar <ananthravikumar@gmail.com>
2023-02-25 00:13:42 -0500

    saving

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,43 @@
 use std::ops::{Add, Mul};
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Node {
     pub value: i32,
     pub children: Vec<Node>, // had initially set this to be HashSet, but they don't have a iter_mut() method. 
     pub grad: f32,
-    pub backward: fn(&mut Self) -> f64,  
+    pub backward: Backward,  
     pub op: String, 
 }
 
+#[derive(Clone)]
+pub struct Backward {
+    pub backward: fn(&mut Node) -> f64, 
+}
+
+impl Backward {
+    pub fn new() -> Backward {
+        Backward { backward: Backward::default}
+    }
+
+    pub fn default(_: &mut Node) -> f64 {
+        0.0 
+    }
+}
+
+
+// define a Pow trait for Nodes; allows for exponentiation
+pub trait Pow<Node> {
+    fn pow(self, rhs: &Node) -> Node;
+}
+
+impl Pow<Node> for &Node {
+    fn pow(self, rhs: &Node) -> Node {
+        Node::new(self.value.pow(rhs.value as u32)) 
+    }
+}
+
+
+// overload the + and * operators for Nodes
 impl Add for &Node {
     type Output = Node;
     fn add(self, other: &Node) -> Node {
@@ -26,7 +55,7 @@ impl Mul for &Node {
 impl Node {
     pub fn new(value: i32) -> Node  {
         let children: Vec<Node> = Vec::new();
-        Node { value, children, grad: 0.0, op: String::from(""), backward: fn(&mut Self) -> f64 { 0.0 } }
+        Node { value, children, grad: 0.0, op: String::from(""), backward: Backward::new() }
     }
 
     pub fn backward(&mut self) {
@@ -44,6 +73,6 @@ impl Node {
     }
 
     pub fn set_backward(&mut self, backward: fn(&mut Self) -> f64) {
-        self.backward = backward 
+        self.backward = Backward { backward } 
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use micrograd_rust::Node;
+use micrograd_rust::Pow;
 
 
 fn main() {
     let n1 = Node::new(5);
-    let n2 = Node::new(3);
-    let n3 = &n1 + &n2;
-    let n4 = &n1 * &n2;
-    println!("n3: {:?}", n3);
-    println!("n4: {:?}", n4);
-    println!("n3's children: {:?}", n3.children);
-
+    let n2 = Node::new(2);
+    let n3 = &n1 * &n2; 
+    let mut n4 = n3.pow(&Node::new(2));
+    n4.backward(); 
+    println!("n1.grad: {}", n1.grad);
 }
